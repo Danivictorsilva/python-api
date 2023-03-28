797026c06299e6f4ed83ca73c31c918cfb9b7869
Danivictorsilva <danivictorsilva@hotmail.com>
2023-03-27 23:13:18 -0300

    refactor: refactored project structure

---
+++ src/config/Server.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask_restplus import Api
+
+
+class Server():
+    def __init__(self):
+        self.app = Flask(__name__)
+        self.api = Api(self.app,
+                       version='1.0.0',
+                       title='Python API Challenge',
+                       description='An API developed for testing my practical skills on Python. It receives an zip code as imput and return the last 4 days forecast as output.',
+                       doc='/docs')
+    def run(self):
+        self.app.run(debug=True)
